78d7af1e458ce9af7a6ade7ac09ac94a555dce56
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2010-08-04 10:09:28 -0500

    todo in readme

--- ruleta/init.rb
+++ ruleta/init.rb
@@ -1,9 +1,7 @@
 require 'yaml'
-require 'model/table'
-require 'model/roulleteItem'
-require 'model/croupier'
+require 'model/*'
 table_config = YAML.load_file( File.join( '.', 'config', 'table.yml' ) )
-
+Table.new(table_config)
 
 
 
