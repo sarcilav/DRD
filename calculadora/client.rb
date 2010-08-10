caf702e7789830e38ce8ec7d973e3e7f2e7bcd47
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2010-08-10 10:43:46 -0500

    calculadora

---
+++ calculadora/client.rb
@@ -0,0 +1,15 @@
+require 'socket'
+hostname = 'localhost'
+port = '9090'
+
+s = TCPSocket.open hostname, port
+while operation = STDIN.gets
+  operation = operation.split " "
+  a = operation.shift
+  op = operation.shift
+  b = operation.shift
+  s.puts "#{a} #{op} #{b}"
+  line = s.gets   # Read lines from the socket
+  puts line.chop      # And print with platform line terminator
+end
+s.close   
